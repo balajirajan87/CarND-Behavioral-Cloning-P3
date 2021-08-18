97eb7f95f432a3b5b56bf0103e2afc7eafe4fb05
balajirajan87 <77254984+balajirajan87@users.noreply.github.com>
2021-08-18 20:16:21 +0530

    For Review
    For Review

--- Model.py
+++ Model.py
@@ -13,18 +13,17 @@ import numpy as np
 import sklearn
 from keras.models import Model
 import matplotlib.pyplot as plt
+from keras import metrics
 
 samples = []
-with open('./Datasets/Master_driving_log.csv') as csvfile:
+with open('./Datasets/Master_driving_log_new.csv') as csvfile:
     reader = csv.reader(csvfile)
     for line in reader:
         samples.append(line)
 
+samples_shuffled = shuffle(samples)
 
-
-train_samples, validation_samples = train_test_split(samples, test_size=0.2)
-
-
+train_samples, validation_samples = train_test_split(samples_shuffled, test_size=0.2)
 
 def generator(samples, batch_size=32):
     num_samples = len(samples)
@@ -35,7 +34,7 @@ def generator(samples, batch_size=32):
 
             images = []
             angles = []
-            correction = 0.2
+            correction = 0.4
             for batch_sample in batch_samples:
                 for i in range(3):
                     source_path = batch_sample[i]
@@ -74,12 +73,12 @@ train_generator = generator(train_samples, batch_size=batch_size)
 validation_generator = generator(validation_samples, batch_size=batch_size)
 
 # when accuracy reaches ACCURACY_THRESHOLD
-ACCURACY_THRESHOLD = 0.95
+LOSS_THRESHOLD = 0.06
 
 class myCallback(Callback):
     def on_epoch_end(self, epoch, logs={}):
-        if(logs.get('acc') > ACCURACY_THRESHOLD):
-            print("\nReached %2.2f%% accuracy, so stopping training!!" %(ACCURACY_THRESHOLD*100))
+        if(logs.get('val_mean_squared_error') < LOSS_THRESHOLD):
+            print("\nReached %2.2f%% Loss, so stopping training!!" %(LOSS_THRESHOLD*100))
             self.model.stop_training = True
 
 callbacks = myCallback()
@@ -87,18 +86,16 @@ callbacks = myCallback()
 model = Sequential()
 model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
 model.add(Cropping2D(cropping=((50,20),(0,0))))
-model.add(Convolution2D(6,5,5,subsample=(2,2),activation="elu"))
-model.add(AveragePooling2D())
-model.add(Convolution2D(16,5,5,subsample=(2,2),activation="elu"))
-model.add(AveragePooling2D())
+model.add(Convolution2D(24,3,3,subsample=(2,2),activation="elu"))
+model.add(Convolution2D(48,3,3,subsample=(2,2),activation="elu"))
+model.add(MaxPooling2D(pool_size=(4, 4), strides=4))
+model.add(Dropout(0.25))
 model.add(Flatten())
-model.add(Dense(120,activation="elu"))
-model.add(Dense(84,activation="elu"))
-model.add(Dense(10,activation="elu"))
+model.add(Dense(50,activation="elu"))
+model.add(Dense(10))
 model.add(Dense(1))
-opt = adam(lr=0.0001)
-model.compile(loss='mse', optimizer=opt, metrics=['accuracy'])
-model.fit_generator(train_generator,steps_per_epoch=np.ceil(len(train_samples)/batch_size),validation_data=validation_generator,validation_steps=np.ceil(len(validation_samples)/batch_size),epochs=10, verbose=1, callbacks=[callbacks])
+model.compile(loss='mse', optimizer='adam',metrics=[metrics.mean_squared_error])
+model.fit_generator(train_generator,steps_per_epoch=np.ceil(len(train_samples)/batch_size),validation_data=validation_generator,validation_steps=np.ceil(len(validation_samples)/batch_size),epochs=20, verbose=1, callbacks=[callbacks])
 print("saving the model")
 model.save('model.h5')
 exit()
